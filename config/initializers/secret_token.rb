aa611cb36f863389826dec8058a5b53acd53d392
zfilm <morten@z-film.no>
2013-10-05 00:05:04 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PositivKompetanse::Application.config.secret_token = '549655dea5010079b0fcf7a7e45f3596aec8dd3e8aa4a45749dfefb2087afcaec1e335a762c38b5434d7b7bf20bee15820cd22d60684b1402f7ee29326b30346'
