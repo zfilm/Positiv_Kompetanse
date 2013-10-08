c8ef414a8cb04e894f6952a2f7ce2a1a0674f7c7
zfilm <morten@z-film.no>
2013-10-08 23:46:01 +0200

    up to 6.3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924110349) do
+ActiveRecord::Schema.define(version: 20131008213858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,5 @@ ActiveRecord::Schema.define(version: 20130924110349) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
